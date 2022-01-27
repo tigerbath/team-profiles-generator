dfa7385eea95a45d9d05ea175136656bae8ab360
Matthew Palmer <mattjpalmer92@gmail.com>
2022-01-27 17:18:46 +0000

    require packages

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// require packages
+
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./lib/generateHTML");
