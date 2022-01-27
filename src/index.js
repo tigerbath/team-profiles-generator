624bbdd4a82cf13430b3653fb05880846e7893b2
Matthew Palmer <mattjpalmer92@gmail.com>
2022-01-27 17:24:22 +0000

    edit intern

--- src/index.js
+++ src/index.js
@@ -92,32 +92,37 @@ const addEngineer = async () => {
 };
 
 const addIntern = async () => {
-    const internQuestions = [
-      {
-        type: "input",
-        name: "name",
-        message: "Please enter the intern name",
-        validate: validateInput,
-      },
-  
-      {
-        type: "input",
-        name: "id",
-        message: "Please enter the intern id",
-        validate: validateInput,
-      },
-  
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the intern email address",
-        validate: validateInput,
-      },
-  
-      {
-        type: "input",
-        name: "school",
-        message: "Please enter the intern school name",
-        validate: validateInput,
-      },
-    ];
+  const internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter the intern name",
+      validate: validateInput,
+    },
+
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the intern id",
+      validate: validateInput,
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter the intern email address",
+      validate: validateInput,
+    },
+
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter the intern school name",
+      validate: validateInput,
+    },
+  ];
+
+  const { name, id, email, school } = await inquirer.prompt(internQuestions);
+  const intern = new Intern(name, id, email, school);
+  return intern;
+};
