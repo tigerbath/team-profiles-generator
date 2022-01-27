bad6ca3fbd4a36a9a05a3bf8e1ca052d5f522825
Matthew Palmer <mattjpalmer92@gmail.com>
2022-01-27 17:37:35 +0000

    tests working & images validating this statement

--- src/lib/Manager.test.js
+++ src/lib/Manager.test.js
@@ -1,13 +1,28 @@
-describe('Manager', () => {
-  test('should be an instance of Manager', () => {});
+const Manager = require("../lib/manager");
 
-  test('should return the expected name', () => {});
+describe("Manager", () => {
+  describe("constructor tests", () => {
+    test("should construct manager object with name, id, email,role and officeNumber", () => {
+      const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
+      expect(manager).toEqual({
+        role: "Manager",
+        name: "Sam",
+        id: 222,
+        email: "sam22@gmail.com",
+        officeNumber: "2222222",
+      });
+    });
+  });
 
-  test('should return the expected id', () => {});
+  describe("method tests", () => {
+    test("should return role when getRole is called", () => {
+      const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
+      expect(manager.getRole()).toEqual("Manager");
+    });
+  });
 
-  test('should return the expected email', () => {});
-
-  test('should return the expected office number', () => {});
-
-  test('should return the expected role', () => {});
+  test("should return officeNumber when getOfficeNumber is called", () => {
+    const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
+    expect(manager.getOfficeNumber()).toEqual("2222222");
+  });
 });
