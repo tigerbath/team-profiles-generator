bffa217740d61ea62f1c9cd4e04e94c512427671
Matthew Palmer <mattjpalmer92@gmail.com>
2022-01-27 17:29:50 +0000

    functionality

--- src/lib/Employee.js
+++ src/lib/Employee.js
@@ -1,11 +1,26 @@
 class Employee {
-  constructor({ name, id, email }) {}
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = "Employee";
+  }
 
-  getName() {}
+  getName() {
+    return this.name;
+  }
 
-  getId() {}
+  getId() {
+    return this.id;
+  }
 
-  getEmail() {}
+  getEmail() {
+    return this.email;
+  }
 
-  getRole() {}
+  getRole() {
+    return this.role;
+  }
 }
+
+module.exports = Employee;
